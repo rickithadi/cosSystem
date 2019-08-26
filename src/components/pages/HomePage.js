bf24ae425f5770ca9bcf58c47d3819cbf00f1d46
rickithadi <rickithadi@gmail.com>
2019-08-26 20:56:17 +0000

    ok forms wokr

--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -4,32 +4,57 @@ import React from 'react';
 
 // IMPORT IMAGES
 
-import react from '../../images/react-small.png';
-import reactrouter from '../../images/react-router-small.png';
-import redux from '../../images/redux-small.png';
-import bootstrap from '../../images/bootstrap4-small.png';
-import sass from '../../images/sass-small.png';
-import webpack from '../../images/webpack-small.png';
-
-
 // COMPONENT
 
 const HomePage = () => (
     <main>
         <div className="jumbotron jumbotron-fluid text-dark bg-light animated fadeIn">
-            <h1 className="display-6 text-center">React Redux Starter</h1>
-            <p className="lead text-center">The essential elements required to start building apps using React, React Router, Redux, Bootstrap 4, SASS, and Webpack</p>
+          <h1 className="display-6 text-center">wadio form</h1>
             <hr className="my-4" />
-            <div className="text-center">
-                <img className="m-3" height="50" src={react} alt="React" />
-                <img className="m-3" height="50" src={reactrouter} alt="React Router" />
-                <img className="m-3" height="50" src={redux} alt="Redux" />
-                <img className="m-3" height="50" src={bootstrap} alt="Bootstrap 4" />
-                <img className="m-3" height="50" src={sass} alt="Sass" />
-                <img className="m-3" height="50" src={webpack} alt="Webpack" />
-            </div>
-        </div>
+                <form>
+                    <div className="form-group">
+                        <label htmlFor="exampleFormControlInput1">Email address</label>
+                        <input
+                            type="email"
+                            className="form-control"
+                            id="exampleFormControlInput1"
+                            placeholder="name@example.com"></input>
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="exampleFormControlSelect1">Example select</label>
+                        <select className="form-control" id="exampleFormControlSelect1">
+                            <option>1</option>
+                            <option>2</option>
+                            <option>3</option>
+                            <option>4</option>
+                            <option>5</option>
+                        </select>
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="exampleFormControlSelect2">
+              Example multiple select
+                        </label>
+                        <select
+                            multiple
+                            className="form-control"
+                            id="exampleFormControlSelect2">
+                            <option>1</option>
+                            <option>2</option>
+                            <option>3</option>
+                            <option>4</option>
+                            <option>5</option>
+                        </select>
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="exampleFormControlTextarea1">Example textarea</label>
+                        <textarea
+                            className="form-control"
+                            id="exampleFormControlTextarea1"
+                            rows="3"></textarea>
+                    </div>
+                </form>
+                </div>
     </main>
 );
 
-export { HomePage };
+export {HomePage};
