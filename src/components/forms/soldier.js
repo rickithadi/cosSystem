5944fbb652306ec98cde93369d1c0a1c4a013841
rickithadi <rickithadi@gmail.com>
2019-08-27 01:21:15 +0000

    hit the woah

--- src/components/forms/soldier.js
+++ src/components/forms/soldier.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
 // IMPORT PROJECT REFERENCES
 import {Form} from 'formsy-react-components';
+import makeAnimated from 'react-select/animated';
+import Select from 'react-select';
 //import {LoadingIndicator} from '../shared/LoadingIndicator/LoadingIndicator';
 //import {Error} from '../shared/Error/Error';
 
@@ -13,8 +15,9 @@ class SoldierForm extends Component {
   state = {
     appointment: '',
     Aappointment: '',
-    milestone: '',
-    achievement: '',
+    milestone: [],
+    achievement: [],
+    offences: '',
     company: '',
     rank: '',
     comd: false,
@@ -31,6 +34,10 @@ class SoldierForm extends Component {
     console.log(this.state, e);
   };
 
+  handleChange = selectedOption => {
+    this.setState({selectedOption});
+    console.log(`Option selected:`, selectedOption);
+  };
   componentDidMount() {
     //this.props.fetchZipCodes();
   }
@@ -41,14 +48,57 @@ class SoldierForm extends Component {
       rank,
       name,
       surname,
-      /*      appointment,*/
-      //Aappointment,
-      //milestone,
-      //achievement,
-      //company,
+      appointment,
+      Aappointment,
+      milestone,
+      achievement,
+      offences,
+      company,
       /*comd,*/
     } = this.state;
+    const animatedComponents = makeAnimated();
     const ranks = [
+      {value: '00', label: 'Private'},
+      {value: '01', label: 'Lance Corporal '},
+      {value: '02', label: 'Corporal '},
+      {value: '03', label: 'Corporal First Class'},
+      {value: '04', label: 'Option E-1'},
+      {value: '05', label: 'Option E-2'},
+    ];
+    const companies = [
+      {value: '00', label: 'Private'},
+      {value: '01', label: 'Lance Corporal '},
+      {value: '02', label: 'Corporal '},
+      {value: '03', label: 'Corporal First Class'},
+      {value: '04', label: 'Option E-1'},
+      {value: '05', label: 'Option E-2'},
+    ];
+    const appointments = [
+      {value: '00', label: 'Private'},
+      {value: '01', label: 'Lance Corporal '},
+      {value: '02', label: 'Corporal '},
+      {value: '03', label: 'Corporal First Class'},
+      {value: '04', label: 'Option E-1'},
+      {value: '05', label: 'Option E-2'},
+    ];
+    const Aappointments = [
+      {value: '00', label: 'Private'},
+      {value: '01', label: 'Lance Corporal '},
+      {value: '02', label: 'Corporal '},
+      {value: '03', label: 'Corporal First Class'},
+      {value: '04', label: 'Option E-1'},
+      {value: '05', label: 'Option E-2'},
+    ];
+    const achievements = [
+      {value: '00', label: 'Private'},
+      {value: '01', label: 'Lance Corporal '},
+      {value: '02', label: 'Corporal '},
+      {value: '03', label: 'Corporal First Class'},
+      {value: '04', label: 'Option E-1'},
+      {value: '05', label: 'Option E-2'},
+    ];
+
+    const milestones = [
       {value: '00', label: 'Private'},
       {value: '01', label: 'Lance Corporal '},
       {value: '02', label: 'Corporal '},
@@ -68,10 +118,13 @@ class SoldierForm extends Component {
                 name="rank"
                 className="form-control"
                 placeholder="rank"
-                value={rank.value}
+                value={rank}
                 onChange={this.onChange}
                 required>
-                {ranks.map((rank) => {
+                <option disabled hidden value="">
+                  Rank
+                </option>
+                {ranks.map(rank => {
                   return (
                     <option key={rank.value} value={rank.label}>
                       {rank.label}
@@ -106,13 +159,129 @@ class SoldierForm extends Component {
               </div>
             </div>
           </div>
+          <div className="form-row">
+            <div className="col-md-2 mb-3">
+              <select
+                name="company"
+                className="form-control"
+                placeholder="company"
+                value={company}
+                onChange={this.onChange}
+                required>
+                <option disabled hidden value="">
+                  Company
+                </option>
+                {companies.map(company => {
+                  return (
+                    <option key={company.value} value={company.label}>
+                      {company.label}
+                    </option>
+                  );
+                })}{' '}
+              </select>
+            </div>
+            <div className="col-md-5 mb-3">
+              <select
+                name="appointment"
+                className="form-control"
+                placeholder="appointment"
+                value={appointment}
+                onChange={this.onChange}
+                required>
+                <option disabled hidden value="">
+                  Appointment
+                </option>
+                {appointments.map(app => {
+                  return (
+                    <option key={app.value} value={app.label}>
+                      {app.label}
+                    </option>
+                  );
+                })}{' '}
+              </select>
+            </div>{' '}
+            <div className="col-md-5 mb-3">
+              <select
+                name="Aappointment"
+                className="form-control"
+                placeholder="Aappointment"
+                value={Aappointment}
+                onChange={this.onChange}>
+                <option disabled hidden value="">
+                  Additional Appointment
+                </option>
+                {Aappointments.map(app => {
+                  return (
+                    <option key={app.value} value={app.label}>
+                      {app.label}
+                    </option>
+                  );
+                })}{' '}
+              </select>
+            </div>{' '}
+          </div>
+          <div className="form-row">
+            <div className="col-md-6 mb-3">
+              <Select
+                name="achievements"
+                className="form-control"
+                placeholder="achievements"
+                closeMenuOnSelect={false}
+                components={animatedComponents}
+                value={achievement}
+                isMulti
+                //onChange={this.onChange}
+                options={achievements}
+                onChange={this.handleChange}></Select>
+            </div>{' '}
+            <div className="col-md-6 mb-3">
+              <select
+                name="milestones"
+                multiple
+                className="form-control"
+                placeholder="milestones"
+                value={milestone}
+                onChange={this.handleChange}>
+                <option disabled hidden value="">
+                  Milestones
+                </option>
+                {milestones.map(app => {
+                  return (
+                    <option key={app.value} value={app.label}>
+                      {app.label}
+                    </option>
+                  );
+                })}{' '}
+              </select>
+            </div>{' '}
+          </div>
 
           <div className="form-row">
-            <button className="btn btn-lg btn-primary btn-block" type="submit">
-              Create Account
-            </button>
+            <div className="col-md-10 mb-3  centered ">
+              <textarea
+                className="form-control"
+                rows="3"
+                type="textArea"
+                name="offences"
+                placeholder="offences"
+                value={offences}
+                onChange={this.onChange}></textarea>
+            </div>
           </div>
+
+          <button className="btn btn-lg btn-primary btn-block" type="submit">
+            Create Account
+          </button>
         </Form>
+  <Select
+                placeholder="achievements"
+                closeMenuOnSelect={false}
+                components={animatedComponents}
+                isMulti
+                //onChange={this.onChange}
+                options={achievements}
+                onChange={this.handleChange}></Select>
+            
       </div>
     );
   }
